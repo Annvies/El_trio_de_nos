803df7d53e7928866b1556d7fd954b2af8318596
hokoridf-cmd <hokoridf@gmail.com>
2025-10-27 20:33:52 -0400

    commit de avance freddy maydana de intents

--- app/src/main/java/com/example/elTrioDeNos/AyudaView.kt
+++ app/src/main/java/com/example/elTrioDeNos/AyudaView.kt
@@ -1,6 +1,8 @@
 package com.example.elTrioDeNos
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,8 @@ class AyudaView : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
     }
-}
+
+    }
